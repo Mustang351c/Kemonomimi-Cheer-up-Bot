109145a8a31c3cbd2d78eb3baf87421b3ffbcaa1
Mustang351c <billyd.davis@yahoo.com>
2020-01-30 19:42:39 -0600

    idk what i did
    didnt run before. now it does.

--- bot.py
+++ bot.py
@@ -62,190 +62,190 @@ def run_bot(r, bunnygirls = replies.bunnygirls,
 
     # print 'searching replies...'
 
-    for reply in r.inbox.comment_replies(limit=5):
-        if reply.author not in ignored_users:
+        for reply in r.inbox.comment_replies(limit=5):
+            if reply.author not in ignored_users:
 
 ##various joke replies
 
-            if ('loli' in reply.body.lower() and reply.id not in comments_replied_to):
+                if ('loli' in reply.body.lower() and reply.id not in comments_replied_to):
 
-                print 'LOLICON FOUND!!'
+                    print 'LOLICON FOUND!!'
 
-                reply.reply("LOLICE!!! I'd like to report a lolicon!!!")
-
-                comments_replied_to.append(reply.id)
-
-                with open('comments_replied_to.txt', 'a') as f:
-                        f.write(reply.id + '\n')
-
-            for lewd_trigger in ['lewd', 'ecchi']:
-                if (lewd_trigger in reply.body.lower() and reply.id not in comments_replied_to):
-
-                    print 'Ecchi baka found'
-
-                    reply.reply('E-Ecchi B-Ba-Baka!!')
+                    reply.reply("LOLICE!!! I'd like to report a lolicon!!!")
 
                     comments_replied_to.append(reply.id)
 
                     with open('comments_replied_to.txt', 'a') as f:
-                        f.write(reply.id+ '\n')
+                            f.write(reply.id + '\n')
 
-            for all_trigger in ['all ', 'one of each', 'not enough']:
-                if (all_trigger in reply.body.lower() and reply.id not in comments_replied_to):
+                for lewd_trigger in ['lewd', 'ecchi']:
+                    if (lewd_trigger in reply.body.lower() and reply.id not in comments_replied_to):
 
-                    print 'all requested in reply ' + reply.id
+                        print 'Ecchi baka found'
 
-                    reply.reply(all_reply)
+                        reply.reply('E-Ecchi B-Ba-Baka!!')
 
-                    comments_replied_to.append(reply.id)
+                        comments_replied_to.append(reply.id)
 
-                    with open('comments_replied_to.txt', 'a') as f:
-                        f.write(reply.id + '\n')
+                        with open('comments_replied_to.txt', 'a') as f:
+                            f.write(reply.id+ '\n')
+
+                for all_trigger in ['all ', 'one of each', 'not enough']:
+                    if (all_trigger in reply.body.lower() and reply.id not in comments_replied_to):
+
+                        print 'all requested in reply ' + reply.id
+
+                        reply.reply(all_reply)
+
+                        comments_replied_to.append(reply.id)
+
+                        with open('comments_replied_to.txt', 'a') as f:
+                            f.write(reply.id + '\n')
 
 ##requests for specific mimis.
 
-            for bunnygirl_trigger in ['bunnygirl', 'bunny girl', 'bunbun', 'bunny', 'bunnies', 'usagi', 'usamimi']:
-                if (bunnygirl_trigger in reply.body.lower() and reply.id not in comments_replied_to):
+                for bunnygirl_trigger in ['bunnygirl', 'bunny girl', 'bunbun', 'bunny', 'bunnies', 'usagi', 'usamimi']:
+                    if (bunnygirl_trigger in reply.body.lower() and reply.id not in comments_replied_to):
 
-                    print 'bunnygirl requested in reply ' + reply.id
+                        print 'bunnygirl requested in reply ' + reply.id
 
-                    reply.reply(bunnygirl_reply)
+                        reply.reply(bunnygirl_reply)
 
-                    comments_replied_to.append(reply.id)
+                        comments_replied_to.append(reply.id)
 
-                    with open('comments_replied_to.txt', 'a') as f:
-                        f.write(reply.id+ '\n')
+                        with open('comments_replied_to.txt', 'a') as f:
+                            f.write(reply.id+ '\n')
 
-            for catgirl_trigger in ['catgirl', 'cat girl', 'nya', 'neko']:
-                if (catgirl_trigger in reply.body.lower() and reply.id not in comments_replied_to):
+                for catgirl_trigger in ['catgirl', 'cat girl', 'nya', 'neko']:
+                    if (catgirl_trigger in reply.body.lower() and reply.id not in comments_replied_to):
 
-                    print 'catgirl requested in reply ' + reply.id
+                        print 'catgirl requested in reply ' + reply.id
 
-                    reply.reply(catgirl_reply)
+                        reply.reply(catgirl_reply)
 
-                    comments_replied_to.append(reply.id)
+                        comments_replied_to.append(reply.id)
 
-                    with open('comments_replied_to.txt', 'a') as f:
-                        f.write(reply.id+ '\n')
+                        with open('comments_replied_to.txt', 'a') as f:
+                            f.write(reply.id+ '\n')
 
-            for doggirl_trigger in ['doggirl', 'dog girl', 'inu', 'wanwan', 'wan wan']:
-                if (doggirl_trigger in reply.body.lower() and reply.id not in comments_replied_to):
+                for doggirl_trigger in ['doggirl', 'dog girl', 'inu', 'wanwan', 'wan wan']:
+                    if (doggirl_trigger in reply.body.lower() and reply.id not in comments_replied_to):
 
-                    print 'doggirl requested in reply ' + reply.id
+                        print 'doggirl requested in reply ' + reply.id
 
-                    reply.reply(doggirl_reply)
+                        reply.reply(doggirl_reply)
 
-                    comments_replied_to.append(reply.id)
+                        comments_replied_to.append(reply.id)
 
-                    with open('comments_replied_to.txt', 'a') as f:
-                        f.write(reply.id+ '\n')
+                        with open('comments_replied_to.txt', 'a') as f:
+                            f.write(reply.id+ '\n')
 
-            for foxgirl_trigger in ['foxgirl', 'fox girl', 'kitsune', 'kon kon']:
-                if (foxgirl_trigger in reply.body.lower() and reply.id not in comments_replied_to):
+                for foxgirl_trigger in ['foxgirl', 'fox girl', 'kitsune', 'kon kon']:
+                    if (foxgirl_trigger in reply.body.lower() and reply.id not in comments_replied_to):
 
-                    print 'foxgirl requested in reply ' + reply.id
+                        print 'foxgirl requested in reply ' + reply.id
 
-                    reply.reply(foxgirl_reply)
+                        reply.reply(foxgirl_reply)
 
-                    comments_replied_to.append(reply.id)
+                        comments_replied_to.append(reply.id)
 
-                    with open('comments_replied_to.txt', 'a') as f:
-                        f.write(reply.id+ '\n')
+                        with open('comments_replied_to.txt', 'a') as f:
+                            f.write(reply.id+ '\n')
 
-            for horsegirl_trigger in ['horsegirl', 'horse girl', 'uma', 'hinin']:
-                if (horsegirl_trigger in reply.body.lower() and 'human' not in reply.body.lower() and reply.id not in comments_replied_to):
+                for horsegirl_trigger in ['horsegirl', 'horse girl', 'uma', 'hinin']:
+                    if (horsegirl_trigger in reply.body.lower() and 'human' not in reply.body.lower() and reply.id not in comments_replied_to):
 
-                    print 'horsegirl requested in reply ' + reply.id
+                        print 'horsegirl requested in reply ' + reply.id
 
-                    reply.reply(horsegirl_reply)
+                        reply.reply(horsegirl_reply)
 
-                    comments_replied_to.append(reply.id)
+                        comments_replied_to.append(reply.id)
 
-                    with open('comments_replied_to.txt', 'a') as f:
-                        f.write(reply.id+ '\n')
+                        with open('comments_replied_to.txt', 'a') as f:
+                            f.write(reply.id+ '\n')
 
-            for wolfgirl_trigger in ['wolfgirl', 'wolf girl', 'awoo', 'ookami']:
-                if (wolfgirl_trigger in reply.body.lower() and reply.id not in comments_replied_to):
+                for wolfgirl_trigger in ['wolfgirl', 'wolf girl', 'awoo', 'ookami']:
+                    if (wolfgirl_trigger in reply.body.lower() and reply.id not in comments_replied_to):
 
-                    print 'wolfgirl requested in reply ' + reply.id
+                        print 'wolfgirl requested in reply ' + reply.id
 
-                    reply.reply(wolfgirl_reply)
+                        reply.reply(wolfgirl_reply)
 
-                    comments_replied_to.append(reply.id)
+                        comments_replied_to.append(reply.id)
 
-                    with open('comments_replied_to.txt', 'a') as f:
-                        f.write(reply.id+ '\n')
+                        with open('comments_replied_to.txt', 'a') as f:
+                            f.write(reply.id+ '\n')
 
 ##goodbot/badbot replies.
 
-            if ('good bot' in reply.body.lower() and reply.id not in comments_replied_to):
+                if ('good bot' in reply.body.lower() and reply.id not in comments_replied_to):
 
-                #print 'Good bot found in reply ' + reply.id
+                    #print 'Good bot found in reply ' + reply.id
 
-                reply.reply('[Thank You! :)](https://i.imgur.com/P3GRavv.gifv)')
+                    reply.reply('[Thank You! :)](https://i.imgur.com/P3GRavv.gifv)')
 
-                print 'thanked reply ' + reply.id
+                    print 'thanked reply ' + reply.id
 
-                comments_replied_to.append(reply.id)
+                    comments_replied_to.append(reply.id)
 
-                with open('comments_replied_to.txt', 'a') as f:
-                    f.write(reply.id+ '\n')
+                    with open('comments_replied_to.txt', 'a') as f:
+                        f.write(reply.id+ '\n')
 
-            if ('bad bot' in reply.body.lower() and reply.id not in comments_replied_to):
+                if ('bad bot' in reply.body.lower() and reply.id not in comments_replied_to):
 
-                print 'TRASH WAIFU FOUND!!'
+                    print 'TRASH WAIFU FOUND!!'
 
-                reply.reply('[Your waifu](https://www.merriam-webster.com/dictionary/trash)')
+                    reply.reply('[Your waifu](https://www.merriam-webster.com/dictionary/trash)')
 
-                comments_replied_to.append(reply.id)
+                    comments_replied_to.append(reply.id)
 
-                with open('comments_replied_to.txt', 'a') as f:
-                    f.write(reply.id+ '\n')
+                    with open('comments_replied_to.txt', 'a') as f:
+                        f.write(reply.id+ '\n')
 
 ##checks comments in above listed subreddit for triggers
  
     #print 'Searching last 25 comments...'
 
-    for comment in subreddits.comments(limit=50):
-        for trigger in (trigger_phrases):
-             if (trigger in comment.body.lower()  and \
-                comment.id not in comments_replied_to and \
-                comment.author not in (ignored_users)):
- 
-                    #print 'Im sad found in comment' + comment.id
+        for comment in subreddits.comments(limit=50):
+            for trigger in (trigger_phrases):
+                 if (trigger in comment.body.lower()  and \
+                    comment.id not in comments_replied_to and \
+                    comment.author not in (ignored_users)):
+     
+                        #print 'Im sad found in comment' + comment.id
 
-                    comment.reply(random.choice(replies))
+                        comment.reply(random.choice(replies))
 
-                    print 'replied to comment ' + comment.id
+                        print 'replied to comment ' + comment.id
 
-                    comments_replied_to.append(comment.id)
-         
-                    with open('comments_replied_to.txt', 'a') as f:
-                        f.write(comment.id + '\n')
+                        comments_replied_to.append(comment.id)
+             
+                        with open('comments_replied_to.txt', 'a') as f:
+                            f.write(comment.id + '\n')
 
 #checks inbox mentions for triggers
 
     #print 'searching mentions...'
 
-    for mention in r.inbox.mentions(limit=5):
-        for trigger in (trigger_phrases):
-            if (trigger in mention.body.lower() and \
-                mention.id not in comments_replied_to and \
-                mention.author not in (ignored_users)):
+        for mention in r.inbox.mentions(limit=5):
+            for trigger in (trigger_phrases):
+                if (trigger in mention.body.lower() and \
+                    mention.id not in comments_replied_to and \
+                    mention.author not in (ignored_users)):
 
 
-                    mention.reply(random.choice(replies))
+                        mention.reply(random.choice(replies))
 
-                    print 'Replied to mention ' + mention.id
+                        print 'Replied to mention ' + mention.id
 
-                    comments_replied_to.append(mention.id)
+                        comments_replied_to.append(mention.id)
 
-                    with open('comments_replied_to.txt', 'a') as f:
-                        f.write(mention.id + '\n')
-    
+                        with open('comments_replied_to.txt', 'a') as f:
+                            f.write(mention.id + '\n')
+        
     #print 'Time for a cat-nap!...'
 
-    time.sleep(60)
+        time.sleep(60)
 
 ##reads file of comment ID's bot has replied to
 def get_saved_comments():
